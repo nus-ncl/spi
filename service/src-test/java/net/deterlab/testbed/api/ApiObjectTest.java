73d029ea00eb16674946d7e1757b74d93bd467a1
teye <a0097953@gmail>
2016-04-08 09:53:07 +0800

    add unit test for ApiObject (#2)

---
+++ service/src-test/java/net/deterlab/testbed/api/ApiObjectTest.java
@@ -0,0 +1,58 @@
+package net.deterlab.testbed.api;
+
+import mockit.Mock;
+import mockit.MockUp;
+import mockit.Mocked;
+import mockit.integration.junit4.JMockit;
+import net.deterlab.testbed.api.DeterFault;
+import org.hamcrest.CoreMatchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import static org.hamcrest.CoreMatchers.*;
+
+@RunWith(JMockit.class)
+public class ApiObjectTest {
+    
+    @Test
+    public void dateToStringNull() {
+        assertThat(ApiObject.dateToString(null), is(equalTo(null)));
+    }
+    
+    @Test
+    public void stringToDateNull() {
+        assertThat(ApiObject.stringToDate(null), is(equalTo(null)));
+    }
+    
+    @Test
+    public void stringToDateValid() {
+        // Convert Apr 01 2016 17:00 GMT+0000 to SGT
+        Date myDateResult = ApiObject.stringToDate("20160401T170000Z");
+        SimpleDateFormat isoFormat = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'");
+        isoFormat.setTimeZone(TimeZone.getTimeZone("GMT+0000"));
+        Date myDateExpected = new Date();
+        try {
+            myDateExpected = isoFormat.parse("20160401T170000Z");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        assertThat(myDateResult, is(equalTo(myDateExpected)));
+    }
+    
+    @Test
+    public void dateToStringValid() {
+        Date myDateResult = ApiObject.stringToDate("20160401T170000Z");
+        String myDateStrResult = ApiObject.dateToString(myDateResult);
+        String myDateStrExpected = "20160401T170000Z";
+        assertThat(myDateStrResult, is(myDateStrExpected));
+    }
+}
